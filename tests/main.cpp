1a7f6f56b58ca33c76d2ac1d249c2a3c3da611e0
Simi4 <Maxim.like.Python@ya.ru>
2022-06-06 14:53:08 +0300

    Add check in test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,20 @@
+#include <filesystem>
+#include <iostream>
 #include "spacebin.h"
 
 int main() {
-  auto stream = std::ifstream("D:\\Projects\\spacebin\\space.bin",
-                              std::ifstream::binary | std::ifstream::in);
+  namespace fs = std::filesystem;
+  auto spacebin_path = fs::path("../tests/space.bin");
+  if (!fs::exists(spacebin_path)) {
+    std::cerr << spacebin_path << " doesn't exist!" << std::endl;
+    return 1;
+  }
+  auto stream =
+      std::ifstream(spacebin_path, std::ifstream::binary | std::ifstream::in);
+  if (!stream) {
+    std::cerr << spacebin_path << " couldn't open!" << std::endl;
+    return 1;
+  }
   SpaceBin s(stream);
   s.GetBWST();
   s.GetBWAL().GetEntries();
